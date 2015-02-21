4127e52ad8155727955f821641677cdd2ace3348
starforce86 <starforce86714@gmail.com>
2015-02-22 00:49:54 +0800

    Add index page

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 var express = require('express');
+var handlebars = require('express-handlebars');
+
 var app = express();
 
 app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
 
-app.get('/', function(request, response) {
-  response.send('Hello World!');
+app.engine('handlebars', handlebars());
+app.set('view engine', 'handlebars');
+
+app.get('/', function(req, res) {
+    res.render('index', { action: 'search'} );
 });
 
 app.listen(app.get('port'), function() {
---
+++ public/js/functions.js
@@ -0,0 +1,9 @@
+/*!
+ * Cloud Hound v1
+ * Copyright 2014 Cloud Hound
+ */
+
+// Highlight the top nav as scrolling occurs
+$('body').scrollspy({
+    target: '.navbar-fixed-top'
+})
